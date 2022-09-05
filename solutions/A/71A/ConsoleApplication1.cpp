ee4e4a667c44c569f07df5c595c7bc42b4250ab7
xopose <30976652+xopose@users.noreply.github.com>
2022-09-05 10:52:41 +0300

    Add files via upload

---
+++ solutions/A/71A/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string input, result;
+    short size;
+    cin >> size;
+    for (size_t i = 0; i < size; i++) {
+    cin >> input;
+    if (input.size() > 10) {
+        cout << input[0] << input.size() - 2 << input[input.size() - 1] << endl;
+
+    }
+    else {
+        cout << input << endl;
+    }
+    }
+    return 0;
+}
